26b7cceafb2435cb01b7d0d446deb9daf4c1afbe
Lunalik <40746746+lunalik2@users.noreply.github.com>
2018-11-27 16:29:01 -0500

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -3,12 +3,14 @@
 
 #include <switch.h>
 
-int main(int argc, char **argv)
+int main()
 {
-    gfxInitDefault();
-    consoleInit(NULL);
-    bpcInitialize();
-    bpcRebootSystem();
+    sdmmc_storage_t sd_storage;
+    f_mount(NULL, "", 1);
+	sdmmc_storage_end(&sd_storage);
+    #ifdef MENU_LOGO_ENABLE
+	    free(Kc_MENU_LOGO);
+    #endif
+    i2c_send_byte(I2C_5, MAX77620_I2C_ADDR, MAX77620_REG_ONOFFCNFG1, MAX77620_ONOFFCNFG1_PWR_OFF);
     return 0;
 }
-
